21e788994c3867eb2c2f32fd9bf2f3639eeb82e4
Henry Jackson <henry.jackson.95@gmail.com>
2018-08-07 14:09:18 -0400

    adds ORM

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ from peewee import *
 
 DATABASE = SqliteDatabase('twocans.db')
 
+
 # Model going second means the class is ultimately a Model
 class User(UserMixin, Model):
     username = CharField(unique=True)
@@ -26,6 +27,26 @@ class User(UserMixin, Model):
             (Post.user == self)
         )
 
+    def following(self):
+        """The users that we are following."""
+        return (
+            User.select().join(
+                Relationship, on=Relationship.to_user
+            ).where(
+                Relationship.from_user == self
+            )
+        )
+
+    def followers(self):
+        """Get users following the current user."""
+        return (
+            User.select().join(
+                Relationship, on=Relationship.from_user
+            ).where(
+                Relationship.to_user == self
+            )
+        )
+
     @classmethod
     def create_user(cls, username, email, password, admin=False):
         try:
@@ -39,6 +60,7 @@ class User(UserMixin, Model):
         except IntegrityError:
             raise ValueError("User already exists")
 
+
 class Post(Model):
     timestamp = DateTimeField(default=datetime.datetime.now)
     user = ForeignKeyField(
@@ -51,7 +73,19 @@ class Post(Model):
         database = DATABASE
         order_by = ('-timestamp', )
 
+
+class Relationship(Model):
+    from_user = ForeignKeyField(User, related_name='relationships')
+    to_user = ForeignKeyField(User, related_name='related_to')
+
+    class Meta:
+        database = DATABASE
+        indexes = (
+            (('from_user', 'to_user'), True)
+        )
+
+
 def initialize():
     DATABASE.connect()
-    DATABASE.create_tables([User, Post], safe=True)
+    DATABASE.create_tables([User, Post, Relationship], safe=True)
     DATABASE.close()
